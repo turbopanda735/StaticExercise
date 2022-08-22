3215bc9869c1d5c80e5ea0f2b31e04e15cce8152
Joel <franco4prez@gmail.com>
2022-08-22 12:01:35 -0400

    completed exercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(74));
+            Console.WriteLine(TempConverter.FahrenheitToKelvin(74));
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(23));
+            Console.WriteLine(TempConverter.CelsiusToKelvin(23));
+            Console.WriteLine(TempConverter.KelvinToFahrenheit(296.483));
+            Console.WriteLine(TempConverter.KelvinToCelsius(296.483));
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double degF)
+        {
+            return (degF - 32) * 5 / 9;
+        }
+        public static double FahrenheitToKelvin(double degF)
+        {
+            return (degF - 32) * 5 / 9 + 273.15;
+        }
+        public static double CelsiusToFahrenheit(double degC)
+        {
+            return degC * 9 / 5 + 32;
+        }
+        public static double CelsiusToKelvin(double degC)
+        {
+            return degC + 273.15;
+        }
+        public static double KelvinToFahrenheit(double degK)
+        {
+            return (degK - 273.15) * 9 / 5 + 32;
+        }
+        public static double KelvinToCelsius(double degK)
+        {
+            return degK - 273.15;
+        }
+    }
+}
